6217ab333bac7934e638145113f18dad376cfc5d
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-03-07 13:21:19 +0100

    Make sure single Sample object will serialize

--- src/sentry/api/serializers/models/sample.py
+++ src/sentry/api/serializers/models/sample.py
@@ -2,9 +2,13 @@ from __future__ import absolute_import
 
 from sentry.api.serializers import Serializer, register
 from sentry.models.sample import Sample
+from django.forms.models import model_to_dict
 
 
 @register(Sample)
 class SampleSerializer(Serializer):
     def serialize(self, obj, attrs, user):
-        return obj.all().values()
+        if isinstance(obj, Sample):
+            return model_to_dict(obj)
+        else:
+            return obj.all().values()
