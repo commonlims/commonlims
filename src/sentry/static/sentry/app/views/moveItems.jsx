9b22ca698262aa3ad4e8c7998558144bfb9723af
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2019-05-16 13:04:07 +0200

    Megacleanout

--- src/sentry/static/sentry/app/views/moveItems.jsx
+++ src/sentry/static/sentry/app/views/moveItems.jsx
@@ -22,10 +22,6 @@ import UserTaskStore from 'app/stores/userTaskStore';
 // TODO in transition:
 //   [x] Rename container after placing samples
 
-const Hidden = {
-  display: 'none',
-};
-
 class Container {
   constructor(id, name, dimensions, typeName, isTemporary) {
     // TODO: This object should only contain data, not viewData (like focus row), so it can be easily updated
@@ -50,17 +46,6 @@ class Container {
         this.locations[key] = new Location(this, r, c);
       }
     }
-  }
-
-  toContract() {
-    // maps back to a simple json object that can be used with the api
-    return {
-      id: this.id,
-      name: this.name,
-      isTemporary: this.isTemporary,
-      dimensions: this.dimensions,
-      typeName: this.typeName,
-    };
   }
 
   getKey(row, col) {
@@ -99,100 +84,6 @@ class Container {
   }
 }
 
-class Transition {
-  constructor(source, target, type = 'general') {
-    this.source = source;
-    this.target = target;
-    this.type = type;
-  }
-
-  toContract() {
-    return {
-      sourceLocation: {
-        row: this.source.row,
-        col: this.source.col,
-        containerId: this.source.container.id,
-        type: this.type,
-      },
-      targetLocation: {
-        row: this.target.row,
-        col: this.target.col,
-        containerId: this.target.container.id,
-        type: this.type,
-      },
-    };
-  }
-}
-
-class Transitions {
-  // Handles mappings between source and targets and provides a simple way
-  // to query for those mappings
-  constructor() {
-    this._transitions = [];
-    this._mappedBySource = {};
-    this._mappedByTarget = {};
-  }
-
-  createTransition(source, target) {
-    let transition = new Transition(source, target);
-    this._transitions.push(transition);
-
-    if (this._mappedBySource[source.id] == undefined) {
-      this._mappedBySource[source.id] = [];
-    }
-    if (this._mappedByTarget[target.id] == undefined) {
-      this._mappedByTarget[target.id] = [];
-    }
-
-    this._mappedBySource[source.id].push(transition);
-    this._mappedByTarget[target.id].push(transition);
-
-    // TODO: Redesign? Thinking about keeping samples/analytes in the source containers and only transitions in the
-    // target object. But that Transition object should probably be a TransitionCollection object or something, because
-    // we are going to support n..n
-    target.content = transition; // TODO: Consider using the transitions instead
-
-    source.transitions.push(transition);
-    target.transitions.push(transition);
-  }
-
-  removeTransition(transition) {
-    this._transitions = this._transitions.filter(item => item != transition);
-    transition.source.highlightTransition = false;
-    transition.target.highlightTransition = false;
-
-    this._mappedBySource[transition.source.id] = this._mappedBySource[
-      transition.source.id
-    ].filter(item => item != transition);
-    this._mappedByTarget[transition.target.id] = this._mappedByTarget[
-      transition.target.id
-    ].filter(item => item != transition);
-
-    // TODO: n..n
-    transition.target.content = null;
-
-    transition.source.transitions = transition.source.transitions.filter(
-      item => item != transition
-    );
-    transition.target.transitions = transition.target.transitions.filter(
-      item => item != transition
-    );
-  }
-
-  getByTarget(target) {
-    return this._mappedByTarget[target.id];
-  }
-  getBySource(source) {
-    return this._mappedBySource[source.id];
-  }
-
-  getByLocation(location) {
-    let ret = this.getBySource(location);
-    if (!ret) ret = this.getByTarget(location);
-    return ret;
-  }
-}
-
 class ContainerSetData {
   // An in memory structure for the sample batch. Uses the json from the sample-batch endpoint
   // to construct a heavier object with a simpler api
@@ -201,7 +92,6 @@ class ContainerSetData {
   constructor() {
     this.sourceContainers = [];
     this.targetContainers = [];
-    this.transitions = new Transitions();
   }
 
   static createFromSampleBatchJson(json) {
@@ -252,66 +142,9 @@ class ContainerSetData {
       ret.targetContainers.push(firstTarget);
     }
 
-    for (let transition of json.transitions) {
-      let sloc = transition.sourceLocation;
-      let tloc = transition.targetLocation;
-
-      let source = ret.findLocation(sloc.row, sloc.col, sloc.containerId);
-      let target = ret.findLocation(tloc.row, tloc.col, tloc.containerId);
-      ret.transitions.createTransition(source, target);
-    }
-
-    return ret;
-  }
-
-  findContainer(id) {
-    for (let container of this.sourceContainers) {
-      if (container.id === id) return container;
-    }
-
-    for (let container of this.targetContainers) {
-      if (container.id === id) return container;
-    }
-    return null;
-  }
-
-  findLocation(row, col, containerId) {
-    let container = this.findContainer(containerId);
-    return container.get(row, col);
-  }
-
-  toSampleBatch() {
-    let ret = {};
-    ret.batchId = this.batchId;
-    ret.correlation = this.correlation;
-    ret.containers = [];
-    ret.tempContainers = [];
-    ret.transitions = [];
-    ret.samples = [];
-
-    for (let container of this.sourceContainers) {
-      ret.containers.push(container.toContract());
-
-      for (let location of container.getLocations(x => x.content !== null)) {
-        let sample = location.content;
-
-        let sampleJson = {
-          name: sample.name,
-          id: sample.id,
-          location: location.toContract(),
-        };
-        ret.samples.push(sampleJson);
-      }
-    }
-
-    for (let container of this.targetContainers) {
-      ret.tempContainers.push(container.toContract());
-    }
-
-    for (let transition of this.transitions._transitions) {
-      let transitionJson = transition.toContract();
-      ret.transitions.push(transitionJson);
-    }
+    // TODO: set transitions into state
+    /*for (let transition of json.transitions) {
+    }*/
 
     return ret;
   }
@@ -336,69 +169,19 @@ class MoveItems extends React.Component {
     };
   }
 
-  wellsEqual(well1, well2) {
-    return well1.row == well2.row && well1.col == well2.col;
-  }
-
-  *getSourceLocations(predicate) {
-    // Get all locations in any source container
-    for (let container of this.props.containerSet.sourceContainers) {
-      for (let loc of container.getLocations(predicate)) {
-        yield loc;
-      }
-    }
-  }
-
-  createTransition(source, target) {
-    source.isSelected = false;
-    this.props.containerSet.transitions.createTransition(source, target);
-    this.onTransitionCreated();
-  }
-
-  onTransitionCreated() {}
-
-  onTransitionRemoved() {}
-
-  removeTransitionForTarget(target) {
-    for (let transition of target.transitions) {
-      this.props.containerSet.transitions.removeTransition(transition);
-      this.onTransitionRemoved();
-    }
-    target.isSelected = false;
-  }
-
-  handleLeaveContainer(container) {
-    container.viewLogic.focusCol = null;
-    container.viewLogic.focusRow = null;
-  }
-
-  handleTargetWellClicked(loc, ctrlKey, shiftKey) {
-    let selLocations = [];
-
-    for (let currentLoc of this.getSourceLocations(loc2 => {
-      return loc2.isSelected;
-    })) {
-      selLocations.push(currentLoc);
-    }
-
-    if (loc.getLocationState() == LocationState.NOT_EMPTY_TRANSITION_TARGET) {
-      this.removeTransitionForTarget(loc);
+  onSourceWellClicked(containerId, row, col, sampleId) {
+    // If an empty well was clicked, clear current transition
+    if (!sampleId) {
+      this.setState({currentSampleTransition: null});
       return;
     }
 
-    if (selLocations.length == 1) {
-      this.createTransition(selLocations[0], loc);
-    }
-  }
-
-  // Change the source location of the current transition, if the well contains a sample
-  onSourceWellClicked(containerId, row, col) {
-    console.log('SOURCE WELL CLICKED', containerId, row, col);
+    // TODO: setCurrentSampleTransition()
   }
 
   // Complete the current transition if appropriate
-  onTargetWellClicked(containerId, row, col) {
-    console.log('TARGET WELL CLICKED', containerId, row, col);
+  onTargetWellClicked(containerId, row, col, sampleId) {
+    console.log('TARGET WELL CLICKED', containerId, row, col, sampleId);
   }
 
   // For now we assume all samples fetched are mapped to source containers.
@@ -438,15 +221,7 @@ class MoveItems extends React.Component {
 }
 
 MoveItems.propTypes = {
-  containerSet: PropTypes.shape({
-    sourceContainers: PropTypes.array.isRequired,
-    targetContainers: PropTypes.array.isRequired,
-    transitions: PropTypes.shape({
-      getByLocation: PropTypes.func,
-      removeTransition: PropTypes.func,
-      createTransition: PropTypes.func,
-    }),
-  }),
+  userTask: PropTypes.shape, // TODO: specify format
 };
 
 export default MoveItems;
