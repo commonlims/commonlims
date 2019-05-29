d548a2958707d4bbbaebc4f475a0cd0fc674a09c
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-05-29 11:08:31 +0200

    Test for the UserTaskSerializer

---
+++ tests/clims/api/serializers/models/test_user_task.py
@@ -0,0 +1,22 @@
+
+from __future__ import absolute_import
+
+from sentry.testutils import TestCase
+
+from clims.api.serializers.models import UserTaskSerializer
+from clims.models.user_task import UserTask
+
+
+class UserTaskSerializerTest(TestCase):
+    def test_can_serialize_task(self):
+
+        model = UserTask(id=1, name="Test1", organization_id=1, handler="somehandler")
+
+        serializer = UserTaskSerializer()
+        result = serializer.serialize(model, None, None)
+        assert result.get('created')
+        assert result.get('handler') == 'somehandler'
+        assert result.get('id') == 1
+        assert result.get('name') == 'Test1'
+        assert result.get('organization') == 1
+        assert result.get('status') == 0
