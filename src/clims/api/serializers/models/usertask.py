acff0ee925df8377fb0807b578c307426bbee983
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-05-29 15:21:54 +0200

    Use Django Serializer
    Explicit is better than implicit

--- src/clims/api/serializers/models/usertask.py
+++ src/clims/api/serializers/models/usertask.py
@@ -1,19 +1,11 @@
 
 from __future__ import absolute_import
 
-from sentry.api.serializers import Serializer, register
-
 from clims.models.user_task import UserTask
 
 from rest_framework import serializers as django_serializers
 
 
-@register(UserTask)
-class UserTaskSerializer(Serializer):
-    def serialize(self, obj, attrs, user):
-        s = self._UserTaskSerializer(obj)
-        return s.data
-
-    class _UserTaskSerializer(django_serializers.ModelSerializer):
-        class Meta:
-            model = UserTask
+class UserTaskSerializer(django_serializers.ModelSerializer):
+    class Meta:
+        model = UserTask
--- src/sentry/api/endpoints/user_task.py
+++ src/sentry/api/endpoints/user_task.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from sentry.api.bases.user_task import UserTaskBaseEndpoint
 from sentry.api.bases.organization import OrganizationEndpoint
 from clims.models import UserTask
+from clims.api.serializers.models.usertask import UserTaskSerializer
 from sentry.models.activity import Activity
-from sentry.api.serializers import serialize
 from sentry.api.paginator import OffsetPaginator
 
 
@@ -17,16 +17,14 @@ class UserTaskEndpoint(OrganizationEndpoint):
             request=request,
             queryset=user_tasks,
             paginator_cls=OffsetPaginator,
-            on_results=lambda x: serialize(x),
+            on_results=lambda x: UserTaskSerializer(x, many=True).data,
         )
 
 
 class UserTaskDetailsEndpoint(UserTaskBaseEndpoint):
     def get(self, request, user_task_id):
         user_task = UserTask.objects.get(pk=user_task_id)
-        return Response(serialize(user_task), status=200)
-
-# TODO This appears to be unused. Can I remove it? /JD 2019-05-29
+        return Response(UserTaskSerializer(user_task).data, status=200)
 
 
 class UserTaskDetailsActivityEndpoint(UserTaskBaseEndpoint):
@@ -43,4 +41,4 @@ class UserTaskDetailsActivityEndpoint(UserTaskBaseEndpoint):
             request=request,
             queryset=activity,
             paginator_cls=OffsetPaginator,
-            on_results=lambda x: serialize(x))
+            on_results=lambda x: UserTaskSerializer(x, many=True).data)
