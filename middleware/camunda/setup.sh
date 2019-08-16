b18bad4a5263129cda14b2fe5714eb80aa095649
Steinar Sturlaugsson <costeinar@gmail.com>
2019-08-16 11:24:18 +0200

    Camunda uses a random password for psql

--- middleware/camunda/setup.sh
+++ middleware/camunda/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# NOTE: Sets up config files and changes the postgres password on every run
+
 mkdir -p ~/.camunda
 
 if [ ! -d ~/.camunda/server ]; then
@@ -13,12 +19,24 @@ else
     echo "Camunda has already been installed"
 fi
 
-echo "Overwriting config files..."
-cp -f ./local-server.xml ~/.camunda/server/apache-tomcat-9.0.19/conf/server.xml
-
 echo "Fetching postgres driver..."
 cd ~/.camunda/server/apache-tomcat-9.0.19/lib
 wget -nc https://jdbc.postgresql.org/download/postgresql-42.2.6.jar
+cd $SCRIPT_PATH
+
+echo 'Creating postgres role...'
+
+set +e
+psql -d clims -c "CREATE ROLE camunda"
+set -e
+
+random_pass=$(openssl rand -base64 32)
+
+psql -d clims -c "ALTER ROLE camunda WITH LOGIN ENCRYPTED PASSWORD '$random_pass'"
+
+echo "Overwriting config files..."
+cp -f ./server.xml ~/.camunda/server/apache-tomcat-9.0.19/conf/server.xml
+perl -pi -e "s#{{ postgres_password }}#$random_pass#g" ~/.camunda/server/apache-tomcat-9.0.19/conf/server.xml
 
 echo 'Server is ready.'
 echo '- Startup: ~/.camunda/server/apache-tomcat-9.0.19/bin/startup.sh'
