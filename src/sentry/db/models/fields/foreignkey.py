ee42a5b3ae7288abd3937c9d237411f092ad309f
Steinar Sturlaugsson <costeinar@gmail.com>
2019-08-22 15:47:00 +0200

    [django 1.9] related_field was renamed to target_field

--- src/sentry/db/models/fields/foreignkey.py
+++ src/sentry/db/models/fields/foreignkey.py
@@ -17,7 +17,7 @@ __all__ = ('FlexibleForeignKey', )
 class FlexibleForeignKey(ForeignKey):
     def db_type(self, connection):
         # This is required to support BigAutoField (or anything similar)
-        rel_field = self.related_field
+        rel_field = self.target_field
         if hasattr(rel_field, 'get_related_db_type'):
             return rel_field.get_related_db_type(connection)
         return super(FlexibleForeignKey, self).db_type(connection)
