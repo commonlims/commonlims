09f96f45203f91edefb312f91e510540909d193b
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2019-08-26 16:46:56 +0200

    Add test for process list item

---
+++ tests/js/spec/components/task/processListItem.spec.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import ProcessListItem from 'app/components/task/processListItem';
+import TaskListItem from 'app/components/task/taskListItem';
+
+describe('ProcessListItem', function() {
+  let mockProps;
+  let processDefinitionKey;
+  let processDefinitionName;
+  let count;
+  let tasks;
+
+  beforeEach(() => {
+    processDefinitionKey = 'process.key';
+    processDefinitionName = 'Process';
+    count = 5;
+    tasks = [];
+
+    mockProps = {
+      processDefinitionKey,
+      processDefinitionName,
+      count,
+      tasks,
+    };
+  });
+
+  const mountProcessListItem = (props = {}) => {
+    const mergedProps = {...mockProps, ...props};
+    return shallow(<ProcessListItem {...mergedProps} />);
+  };
+
+  it('renders a list of tasks', () => {
+    const task1 = {
+      name: 'Task1',
+      count: 2,
+      taskDefinitionKey: 'task1',
+    };
+    const task2 = {
+      name: 'Task2',
+      count: 3,
+      taskDefinitionKey: 'task2',
+    };
+    const wrapper = mountProcessListItem({tasks: [task1, task2]});
+    expect(wrapper.find(TaskListItem).length).toBe(2);
+
+    const taskListItems = wrapper.find(TaskListItem);
+    expect(taskListItems.at(0).props().name).toBe('Task1');
+    expect(taskListItems.at(0).props().count).toBe(2);
+    expect(taskListItems.at(0).props().taskDefinitionKey).toBe('task1');
+    expect(taskListItems.at(1).props().name).toBe('Task2');
+    expect(taskListItems.at(1).props().count).toBe(3);
+    expect(taskListItems.at(1).props().taskDefinitionKey).toBe('task2');
+  });
+});
