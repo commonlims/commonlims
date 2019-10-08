157734c13801daf5874a67ffd1c11946f655d8a3
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2019-10-08 16:36:57 +0200

    Fixup create sample scripts to use new apis

--- src/clims/runner/commands/create_example_data.py
+++ src/clims/runner/commands/create_example_data.py
@@ -1,5 +1,6 @@
 
 from __future__ import absolute_import, print_function
+import random
 
 import click
 from sentry.runner.decorators import configuration
@@ -11,10 +12,32 @@ def createexampledata():
     """
     Create example data for clims
     """
+    from clims.services.application import ioc, ApplicationService
+    from clims.services.substance import SubstanceBase, FloatField
+    from clims.models.plugin_registration import PluginRegistration
+    from sentry.models.organization import Organization
 
-    # Import models here because they need to be wrapped with a Django context to work.
-    # /JD 2019-05-27
-    from clims.models.work_batch import WorkBatch
-    WorkBatch.objects.create(name="Test1", organization_id=1, handler="somehandler")
-    WorkBatch.objects.create(name="Test2", organization_id=1, handler="somehandler2")
-    WorkBatch.objects.create(name="Test3", organization_id=1, handler="somehandler2")
+    app = ApplicationService()
+    ioc.set_application(app)
+
+    org = Organization.objects.get(name="lab")
+
+    example_plugin, _ = PluginRegistration.objects.get_or_create(
+        name='clims.example.plugin', version='1.0.0', organization=org)
+
+    class ExampleSample(SubstanceBase):
+        moxy = FloatField("moxy")
+        cool = FloatField("cool")
+        erudite = FloatField("erudite")
+
+    app.extensibles.register(example_plugin, ExampleSample)
+
+    for _ in range(100):
+        name = "sample-{}".format(random.randint(1, 1000000))
+        sample = ExampleSample(name=name,
+                               organization=org,
+                               moxy=random.randint(1, 100),
+                               cool=random.randint(1, 100),
+                               erudite=random.randint(1, 100))
+        sample.save()
+        click.echo("Created sample: {}".format(sample.name))
