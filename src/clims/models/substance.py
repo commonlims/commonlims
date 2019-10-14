2083edfef636cdef4491267fb23daaa2b6f9b754
Steinar Sturlaugsson <costeinar@gmail.com>
2019-10-14 12:44:29 +0200

    Add location to Substance

--- src/clims/models/substance.py
+++ src/clims/models/substance.py
@@ -30,6 +30,8 @@ class Substance(ExtensibleModel):
 
     project = FlexibleForeignKey('sentry.Project', null=True)
 
+    location = FlexibleForeignKey('clims.Location', null=True)
+
     # The original substance or substances (e.g. in the case of pools) this
     # substance originates from
     origins = models.ManyToManyField('clims.Substance')
