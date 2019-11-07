ded933221456a9223f1352c9a252abbaed60ace0
Steinar Sturlaugsson <costeinar@gmail.com>
2019-11-07 15:58:44 +0100

    Remove unnecessary code from substance model serializer

--- src/clims/api/serializers/models/substance.py
+++ src/clims/api/serializers/models/substance.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-from clims.models.substance import Substance
 from clims.api.serializers.models.extensible_property import ExtensiblePropertySerializer
 
 from rest_framework import serializers
@@ -13,12 +12,3 @@ class SubstanceSerializer(serializers.Serializer):
     name = serializers.CharField()
     properties = DictField(child=ExtensiblePropertySerializer(read_only=True))
     type_full_name = serializers.CharField()
-
-    def create(self, validated_data):
-        return Substance.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-
-        return instance
