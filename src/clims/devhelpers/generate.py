55281f4bab96a0de1317120aca71ab169afeb3f3
Steinar Sturlaugsson <costeinar@gmail.com>
2020-06-10 15:41:54 +0200

    HACKING on code generators

--- src/clims/devhelpers/generate.py
+++ src/clims/devhelpers/generate.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import
 
-import os
-
-from clims.devhelpers import resources
 import re
+import os
 from jinja2 import Template
 import stringcase
+from clims.devhelpers import resources
 
 # TODO: This should be a separate module (e.g. clims_devtools)
 
@@ -36,10 +35,6 @@ class CodeGenerator(object):
         for p in urlpatterns:
             yield self.url_pattern_to_api_call(p.regex.pattern)
 
-    def list_endpoint(self, url):
-        # A list endpoint should support getting all instances and creating a new:
-        pass
-
     def get_template(self, rel_path):
         root = os.path.dirname(resources.__file__)
         res_path = os.path.join(root, rel_path)
@@ -47,45 +42,89 @@ class CodeGenerator(object):
             template = Template(fs.read(), trim_blocks=True, lstrip_blocks=True)
             return template
 
-    def generate(self,
-            resource,
-            single_endpoint,
-            single_parameters,
-            filter_for_single,
-            list_endpoint,
-            list_parameters,
-            ):
-        list_templ = self.get_template("redux/actions.js.j2")
-        resource_const = stringcase.constcase(resource)
+    def generate(self, info):
+        def _generate(root_path, template_name):
+            fname = "{}.js".format(info.resource.camel)
+            path = os.path.join(root_path, fname)
 
-        rendered = list_templ.render(
-            resource=resource,
-            resource_const=resource_const,
-            single_endpoint=single_endpoint,
-            single_parameters=single_parameters,
-            filter_for_single=filter_for_single,
-            list_endpoint=list_endpoint,
-            list_parameters=list_parameters,
-        )
-        return rendered
+            templ = self.get_template(template_name)
+            rendered = templ.render(**info.__dict__)
+            with open(path, "w") as fs:
+                fs.write(rendered)
+
+        _generate(self.actions_path, "redux/actions.js.j2")
+        _generate(self.reducers_path, "redux/reducers.js.j2")
 
     def generate_all(self):
         # Might: Generate from all of our urlpatterns, but for now just hardcoding these:
-        gen_list = [{
-            "resource": "TaskDefinition",
-            "single_endpoint": "/api/0/organizations/${org.slug}/workflow/aggregate/task/",
-            "single_parameters": ["org", "processDefinitionKey", "taskDefinitionKey"],
-            "filter_for_single": ["processDefinitionKey", "taskDefinitionKey"],
-            "list_endpoint": "/api/0/organizations/${org.slug}/workflow/aggregate/task/",
-            "list_parameters": ["org"],
-        }]
+        infos = [
+            # NOTE: This endpoint is peculiar in that we fetch single entries not by id
+            # from a details endpoint, but from the list endpoint with parameters. That's why
+            # we need org for the single endpoint
 
-        for gen_info in gen_list:
-            rendered = self.generate(**gen_info)
-            actions_file = "{}.js".format(stringcase.camelcase(gen_info["resource"]))
-            fpath = os.path.join(self.actions_path, actions_file)
-            with open(fpath, "w") as fs:
-                fs.write(rendered)
+            # TemplateInfo(
+            #     "TaskDefinition",
+            #     single_parameters=["org", "processDefinitionKey", "taskDefinitionKey"],
+            #     single_endpoint="/api/0/organizations/${org.slug}/task-definitions/",
+            #     filter_for_single=["processDefinitionKey", "taskDefinitionKey"],
+            # ),
+            # TemplateInfo(
+            #     "Task",
+            #     list_endpoint="/api/0/organizations/${org.slug}/tasks/",
+            #     list_parameters=["org", "processDefinitionKey", "taskDefinitionKey"],
+            #     list_get_args=["processDefinitionKey", "taskDefinitionKey"],
+            # ),
+            TemplateInfo("ProcessDefinition",
+                list_endpoint="/api/0/process-definitions/",
+                list_parameters=[],
+            )
+        ]
+
+        for info in infos:
+            self.generate(info)
+
+
+class Resource(object):
+    def __init__(self, resource, plural_form):
+        self.resource = resource
+        self.resource_plural = plural_form or self.resource + "s"
+        self.spinal = stringcase.constcase(resource)
+
+        # TODO: We currently use plural for the endpoints. Consider using singular only.
+        self.endpoint = stringcase.spinalcase(self.resource_plural)
+        self.const = stringcase.constcase(resource)
+        self.camel = stringcase.camelcase(resource)
+
+    def __str__(self):
+        return str(self.resource)
+
+
+class TemplateInfo(object):
+    def __init__(self,
+            resource,
+            single_endpoint=None,
+            single_parameters=None,
+            filter_for_single=None,
+            list_endpoint=None,
+            list_parameters=None,
+            list_get_args=None,
+            plural_form=None):
+
+        self.resource = Resource(resource, plural_form)
+        self.single_parameters = single_parameters or ["id"]
+        self.single_signature = self.create_signature(self.single_parameters)
+        self.single_endpoint = single_endpoint or "/api/0/{}/${{id}}".format(self.resource.endpoint)
+        self.filter_for_single = filter_for_single or []
+        self.list_endpoint = list_endpoint or "/api/0/organizations/${{org.slug}}/{}/".format(self.resource.endpoint)
+        self.list_parameters = ["org"] if list_parameters is None else list_parameters
+        self.list_get_args = list_get_args or []
+        self.list_signature = self.create_signature(self.list_parameters)
+
+    def create_signature(self, parameters):
+        signature = ", ".join(parameters)
+        if len(parameters) != 1:
+            signature = "({})".format(signature)
+        return signature
 
 
 if __name__ == "__main__":
