a154afae33cae35756eb0eb5c6c7eb3de4882daa
Steinar Sturlaugsson <costeinar@gmail.com>
2020-06-16 14:55:40 +0200

    Workbatch reducers and actions
    Note that there are no tests for the actions because they are all tested
    in the test for actions/shared

--- src/sentry/static/sentry/app/redux/actions/workBatch.js
+++ src/sentry/static/sentry/app/redux/actions/workBatch.js
@@ -1,49 +1,74 @@
-import axios from 'axios';
+import {Client} from 'app/api';
+import {browserHistory} from 'react-router';
+import {makeResourceActions} from './shared';
 
-export const WORK_BATCHES_GET_REQUEST = 'WORK_BATCHES_GET_REQUEST';
-export const WORK_BATCHES_GET_SUCCESS = 'WORK_BATCHES_GET_SUCCESS';
-export const WORK_BATCHES_GET_FAILURE = 'WORK_BATCHES_GET_FAILURE';
-export const WORK_BATCHES_TOGGLE_SELECT_ALL = 'WORK_BATCHES_TOGGLE_SELECT_ALL';
-export const WORK_BATCH_TOGGLE_SELECT = 'WORK_BATCH_TOGGLE_SELECT';
+export const RESOURCE_NAME = 'WORK_BATCH';
 
-export const workBatchesGetRequest = () => {
+export const workBatchActions = makeResourceActions(
+  RESOURCE_NAME,
+  '/api/0/organizations/lab/work-batches/' // TODO: nolab
+);
+
+//
+// New work batch
+//
+
+export const WORK_BATCHES_CREATE_REQUEST = 'WORK_BATCHES_CREATE_REQUEST';
+export const workBatchesCreateRequest = () => {
   return {
-    type: WORK_BATCHES_GET_REQUEST,
+    type: WORK_BATCHES_CREATE_REQUEST,
   };
 };
 
-export const workBatchesGetSuccess = (workBatches) => {
+export const WORK_BATCHES_CREATE_SUCCESS = 'WORK_BATCHES_CREATE_SUCCESS';
+export const workBatchesCreateSuccess = workBatch => {
   return {
-    type: WORK_BATCHES_GET_SUCCESS,
-    workBatches,
+    type: WORK_BATCHES_CREATE_SUCCESS,
+    workBatch,
   };
 };
 
-export const workBatchesGetFailure = (err) => ({
-  type: WORK_BATCHES_GET_FAILURE,
+export const WORK_BATCHES_CREATE_FAILURE = 'WORK_BATCHES_CREATE_FAILURE';
+export const workBatchesCreateFailure = err => ({
+  type: WORK_BATCHES_CREATE_FAILURE,
   message: err,
 });
 
-export const workBatchesGet = () => (dispatch) => {
-  dispatch(workBatchesGetRequest());
-  // TODO: create a new API client to replace api.jsx
-  // and use axios instead of jquery there
-  return axios
-    .get('/api/0/organizations/lab/work-batches/')
-    .then((res) => dispatch(workBatchesGetSuccess(res.data)))
-    .catch((err) => dispatch(workBatchesGetFailure(err)));
+// Redirect pattern:
+// * Redirects after successful actions are handled in the same actions file
+// * In order not to cause redirects for components that don't require it, redirect only
+//   if the action receives a `redirect: true` flag
+
+export const createWorkBatch = (org, taskIds, redirect) => dispatch => {
+  dispatch(workBatchesCreateRequest());
+
+  const api = new Client();
+  const data = {
+    tasks: taskIds,
+  };
+
+  // TODO: synch work-batches/workbatches
+  api.request(`/api/0/organizations/${org.slug}/work-batches/`, {
+    method: 'POST',
+    data,
+    success: res => {
+      dispatch(workBatchesCreateSuccess(res.workBatch));
+      const createdId = res.workBatch.id;
+      if (redirect) {
+        browserHistory.push(`/${org.slug}/workbatches/${createdId}/`);
+      }
+    },
+    error: err => {
+      dispatch(workBatchesCreateFailure(err));
+    },
+  });
 };
 
-export const workBatchToggleSelect = (id) => {
-  return {
-    type: WORK_BATCH_TOGGLE_SELECT,
-    id,
-  };
-};
-
-export const workBatchesToggleSelectAll = (doSelect) => {
-  return {
-    type: WORK_BATCHES_TOGGLE_SELECT_ALL,
-    doSelect,
-  };
-};
+//
+// Current workbatch
+//
+export const WORK_BATCHES_SELECT_TASK = 'WORK_BATCHES_SELECT_TASK';
+export const workBatchesSelectTask = task => ({
+  type: WORK_BATCHES_SELECT_TASK,
+  task,
+});
