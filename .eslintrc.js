1e101e3f57862437c3577f451d4c11ec3b7efb44
Steinar Sturlaugsson <costeinar@gmail.com>
2020-06-22 14:12:59 +0200

    Upgrade prettier and eslint
    Note that we also stop using eslint-config-sentry-app which means we
    have to move some rules into our config instead, as well as some
    packages to package.json.
    
    Because we're removing this dependency and for simplicity, I'll follow
    this commit up with a `eslint --fix` for everything, which will lead to
    a huge one-time change of the codebase, using default prettier
    rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 /*eslint-env node*/
 module.exports = {
-  extends: ['sentry-app'],
+  extends: ['plugin:prettier/recommended', 'prettier/react'],
+
   globals: {
     require: false,
     expect: false,
@@ -10,7 +11,34 @@ module.exports = {
     tick: true,
     jest: true,
   },
+
   rules: {
-  	"eqeqeq": "off"  // TODO: Enable again
+    eqeqeq: 'off', // TODO: Enable again
   },
+
+  plugins: ['import', 'prettier', 'react', 'jest'],
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      legacyDecorators: true,
+    },
+  },
+
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    jquery: true, // hard-loaded into vendor.js
+  },
+
+  settings: {
+    'import/resolver': 'webpack',
+    'import/extensions': ['.js', '.jsx'],
+  },
+
+  parser: 'babel-eslint',
 };
