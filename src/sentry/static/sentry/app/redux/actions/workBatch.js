6d3d51cc31092568504af1c6a1f86d95d2bb9022
Steinar Sturlaugsson <costeinar@gmail.com>
2020-08-08 22:43:00 +0200

    WorkBatch action/reducer

--- src/sentry/static/sentry/app/redux/actions/workBatch.js
+++ src/sentry/static/sentry/app/redux/actions/workBatch.js
@@ -1,49 +1,11 @@
-import axios from 'axios';
+import {Client} from 'app/api';
+import {browserHistory} from 'react-router';
+import {makeResourceActions} from './shared';
 
-export const WORK_BATCHES_GET_REQUEST = 'WORK_BATCHES_GET_REQUEST';
-export const WORK_BATCHES_GET_SUCCESS = 'WORK_BATCHES_GET_SUCCESS';
-export const WORK_BATCHES_GET_FAILURE = 'WORK_BATCHES_GET_FAILURE';
-export const WORK_BATCHES_TOGGLE_SELECT_ALL = 'WORK_BATCHES_TOGGLE_SELECT_ALL';
-export const WORK_BATCH_TOGGLE_SELECT = 'WORK_BATCH_TOGGLE_SELECT';
+export const RESOURCE_NAME = 'WORK_BATCH';
 
-export const workBatchesGetRequest = () => {
-  return {
-    type: WORK_BATCHES_GET_REQUEST,
-  };
-};
-
-export const workBatchesGetSuccess = (workBatches) => {
-  return {
-    type: WORK_BATCHES_GET_SUCCESS,
-    workBatches,
-  };
-};
-
-export const workBatchesGetFailure = (err) => ({
-  type: WORK_BATCHES_GET_FAILURE,
-  message: err,
-});
-
-export const workBatchesGet = () => (dispatch) => {
-  dispatch(workBatchesGetRequest());
-  // TODO: create a new API client to replace api.jsx
-  // and use axios instead of jquery there
-  return axios
-    .get('/api/0/organizations/lab/work-batches/')
-    .then((res) => dispatch(workBatchesGetSuccess(res.data)))
-    .catch((err) => dispatch(workBatchesGetFailure(err)));
-};
-
-export const workBatchToggleSelect = (id) => {
-  return {
-    type: WORK_BATCH_TOGGLE_SELECT,
-    id,
-  };
-};
-
-export const workBatchesToggleSelectAll = (doSelect) => {
-  return {
-    type: WORK_BATCHES_TOGGLE_SELECT_ALL,
-    doSelect,
-  };
-};
+export const workBatchActions = makeResourceActions(
+  RESOURCE_NAME,
+  '/api/0/organizations/{org}/work-batches/',
+  (org, res) => `/${org.slug}/workbatches/${res.workBatch.id}/`
+);
---
+++ tests/js/spec/redux/reducers/workBatch.spec.js
@@ -0,0 +1,181 @@
+import workBatch, {initialState} from 'app/redux/reducers/workBatch';
+import {workBatchActions} from 'app/redux/actions/workBatch';
+import {Set} from 'immutable';
+
+describe('workBatch reducer', () => {
+  it('should handle getRequest', () => {
+    const state = workBatch(
+      initialState,
+      workBatchActions.getListRequest('search', 'groupby', 'cursor')
+    );
+    const expected = {
+      loadingDetails: false,
+      detailsId: null,
+      loading: true,
+      errorMessage: null,
+      byIds: {},
+      listViewState: {
+        allVisibleSelected: false,
+        groupBy: 'groupby',
+        search: 'search',
+        visibleIds: [],
+        selectedIds: Set(),
+        pagination: {pageLinks: null, cursor: 'cursor'},
+      },
+      creating: false,
+    };
+    expect(state).toEqual(expected);
+  });
+
+  it('should handle getListSuccess', () => {
+    const entries = [TestStubs.WorkBatch(1), TestStubs.WorkBatch(2)];
+    const state = workBatch(
+      initialState,
+      workBatchActions.getListSuccess(entries, 'link')
+    );
+    const expected = {
+      loadingDetails: false,
+      detailsId: null,
+      loading: false,
+      errorMessage: null,
+      byIds: {
+        1: entries[0],
+        2: entries[1],
+      },
+      listViewState: {
+        allVisibleSelected: false,
+        groupBy: null,
+        search: null,
+        visibleIds: [1, 2],
+        selectedIds: Set(),
+        pagination: {pageLinks: 'link'},
+      },
+      creating: false,
+    };
+    expect(state).toEqual(expected);
+  });
+
+  it('should handle getListFailure', () => {
+    const message = 'message';
+    const statusCode = 500;
+    const state = workBatch(initialState, workBatchActions.getListFailure(statusCode, message));
+    const expected = {
+      loadingDetails: false,
+      detailsId: null,
+      loading: false,
+      errorMessage: message,
+      byIds: {},
+      listViewState: {
+        allVisibleSelected: false,
+        groupBy: null,
+        search: null,
+        visibleIds: [],
+        selectedIds: Set(),
+        pagination: {pageLinks: null, cursor: null},
+      },
+      creating: false,
+    };
+    expect(state).toEqual(expected);
+  });
+
+  it('should handle select/deselect for a page', () => {
+    const entries = [TestStubs.WorkBatch(1), TestStubs.WorkBatch(2)];
+    const stateAfterSuccess = workBatch(
+      initialState,
+      workBatchActions.getListSuccess(entries, 'link')
+    );
+    const afterSelectPage = workBatch(
+      stateAfterSuccess,
+      workBatchActions.selectPage(true)
+    );
+    const expectedAfterSelectPage = {
+      loadingDetails: false,
+      detailsId: null,
+      loading: false,
+      errorMessage: null,
+      byIds: {
+        1: entries[0],
+        2: entries[1],
+      },
+      listViewState: {
+        allVisibleSelected: true,
+        groupBy: null,
+        search: null,
+        visibleIds: [1, 2],
+        selectedIds: Set([1, 2]),
+        pagination: {pageLinks: 'link'},
+      },
+      creating: false,
+    };
+    expect(afterSelectPage).toEqual(expectedAfterSelectPage);
+
+    const afterDeselectPage = workBatch(
+      afterSelectPage,
+      workBatchActions.selectPage(false)
+    );
+    const expectedAfterDeselectPage = {
+      ...expectedAfterSelectPage,
+      listViewState: {
+        ...expectedAfterSelectPage.listViewState,
+        allVisibleSelected: false,
+        selectedIds: Set(),
+      },
+    };
+    expect(afterDeselectPage).toEqual(expectedAfterDeselectPage);
+  });
+
+  it('should handle select/deselect for a single entry', () => {
+    const entries = [TestStubs.WorkBatch(1), TestStubs.WorkBatch(2)];
+    const stateAfterSuccess = workBatch(
+      initialState,
+      workBatchActions.getListSuccess(entries, 'link')
+    );
+    const stateAfterSelectFirst = workBatch(
+      stateAfterSuccess,
+      workBatchActions.select(1, true)
+    );
+    const expectedStateAfterSelectFirst = {
+      loadingDetails: false,
+      detailsId: null,
+      loading: false,
+      errorMessage: null,
+      byIds: {
+        1: entries[0],
+        2: entries[1],
+      },
+      listViewState: {
+        allVisibleSelected: false,
+        groupBy: null,
+        search: null,
+        visibleIds: [1, 2],
+        selectedIds: Set([1]),
+        pagination: {pageLinks: 'link'},
+      },
+      creating: false,
+    };
+    expect(stateAfterSelectFirst).toEqual(expectedStateAfterSelectFirst);
+
+    const stateAfterDeselectFirst = workBatch(
+      stateAfterSuccess,
+      workBatchActions.select(1, false)
+    );
+    const expectedStateAfterDeselectFirst = {
+      ...expectedStateAfterSelectFirst,
+      listViewState: {
+        ...expectedStateAfterSelectFirst.listViewState,
+        selectedIds: Set(),
+      },
+    };
+    expect(stateAfterDeselectFirst).toEqual(expectedStateAfterDeselectFirst);
+  });
+
+  it('should handle requesting creation of an entry', () => {
+    const state = workBatch(
+      initialState,
+      workBatchActions.createRequest('search', 'groupby', 'cursor')
+    );
+    console.log(state);
+
+
+  });
+});
