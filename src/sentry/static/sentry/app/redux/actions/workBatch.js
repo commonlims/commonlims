c7530fbd6789b75ea51139da0f543607627da598
Steinar Sturlaugsson <costeinar@gmail.com>
2020-08-08 22:43:00 +0200

    WorkBatch action/reducer

--- src/sentry/static/sentry/app/redux/actions/workBatch.js
+++ src/sentry/static/sentry/app/redux/actions/workBatch.js
@@ -1,49 +1,30 @@
-import axios from 'axios';
+import {Client} from 'app/api';
+import {browserHistory} from 'react-router';
+import {makeResourceActions} from './shared';
 
-export const WORK_BATCHES_GET_REQUEST = 'WORK_BATCHES_GET_REQUEST';
-export const WORK_BATCHES_GET_SUCCESS = 'WORK_BATCHES_GET_SUCCESS';
-export const WORK_BATCHES_GET_FAILURE = 'WORK_BATCHES_GET_FAILURE';
-export const WORK_BATCHES_TOGGLE_SELECT_ALL = 'WORK_BATCHES_TOGGLE_SELECT_ALL';
-export const WORK_BATCH_TOGGLE_SELECT = 'WORK_BATCH_TOGGLE_SELECT';
+export const RESOURCE_NAME = 'WORK_BATCH';
 
-export const workBatchesGetRequest = () => {
-  return {
-    type: WORK_BATCHES_GET_REQUEST,
-  };
-};
+export const workBatchActions = makeResourceActions(
+  RESOURCE_NAME,
+  '/api/0/organizations/{org}/work-batches/'
+);
 
-export const workBatchesGetSuccess = (workBatches) => {
-  return {
-    type: WORK_BATCHES_GET_SUCCESS,
-    workBatches,
-  };
-};
+//
+// New work batch
+//
+// workBatchActions.createSuccess = workBatchesCreateSuccess;
 
-export const workBatchesGetFailure = (err) => ({
-  type: WORK_BATCHES_GET_FAILURE,
-  message: err,
+// Redirect pattern:
+// * Redirects after successful actions are handled in the same actions file
+// * In order not to cause redirects for components that don't require it, redirect only
+//   if the action receives a `redirect: true` flag
+
+//
+// Current workbatch
+//
+export const WORK_BATCHES_SELECT_TASK = 'WORK_BATCHES_SELECT_TASK';
+export const workBatchesSelectTask = (task) => ({
+  type: WORK_BATCHES_SELECT_TASK,
+  task,
 });
-
-export const workBatchesGet = () => (dispatch) => {
-  dispatch(workBatchesGetRequest());
-  // TODO: create a new API client to replace api.jsx
-  // and use axios instead of jquery there
-  return axios
-    .get('/api/0/organizations/lab/work-batches/')
-    .then((res) => dispatch(workBatchesGetSuccess(res.data)))
-    .catch((err) => dispatch(workBatchesGetFailure(err)));
-};
-
-export const workBatchToggleSelect = (id) => {
-  return {
-    type: WORK_BATCH_TOGGLE_SELECT,
-    id,
-  };
-};
-
-export const workBatchesToggleSelectAll = (doSelect) => {
-  return {
-    type: WORK_BATCHES_TOGGLE_SELECT_ALL,
-    doSelect,
-  };
-};
+console.log(workBatchActions);
