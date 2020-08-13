45e9ab1601dd8580ee45a86f588d22084c4e61a4
Edvard Englund <edvard.englund@medsci.uu.se>
2020-08-13 11:35:17 +0200

    trigger expand api call from ui with right params

--- src/sentry/static/sentry/app/views/substances/substances.jsx
+++ src/sentry/static/sentry/app/views/substances/substances.jsx
@@ -27,6 +27,7 @@ class Substances extends React.Component {
     this.onSearch = this.onSearch.bind(this);
     this.toggleAll = this.toggleAll.bind(this);
     this.onCursor = this.onCursor.bind(this);
+    this.expandCollapse = this.expandCollapse.bind(this);
 
     let {search, cursor, groupBy} = this.props.substanceSearchEntry;
     const query = this.props.location.query;
@@ -148,6 +149,11 @@ class Substances extends React.Component {
     );
   }
 
+  expandCollapse(parentEntry) {
+    let {groupBy} = this.props.substanceSearchEntry;
+    this.props.substanceSearchEntryExpandCollapse(parentEntry, groupBy);
+  }
+
   render() {
     // TODO: Rename css classes to something else than stream
     const groupOptions = [
@@ -194,7 +200,7 @@ class Substances extends React.Component {
             toggleAll={this.toggleAll}
             toggleSingle={this.props.substanceSearchEntryToggleSelect}
             listActionBar={actionBar}
-            expandCollapse={this.props.substanceSearchEntryExpandCollapse}
+            expandCollapse={this.expandCollapse}
           />
 
           {this.props.substanceSearchEntry.paginationEnabled &&
@@ -239,8 +245,8 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(substanceSearchEntriesToggleSelectAll(doSelect)),
   substanceSearchEntryToggleSelect: (id, doSelect) =>
     dispatch(substanceSearchEntryToggleSelect(id, doSelect)),
-  substanceSearchEntryExpandCollapse: (parentEntry) =>
-    dispatch(substanceSearchEntryExpandCollapse(parentEntry)),
+  substanceSearchEntryExpandCollapse: (parentEntry, searchKey) =>
+    dispatch(substanceSearchEntryExpandCollapse(parentEntry, searchKey)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Substances);
