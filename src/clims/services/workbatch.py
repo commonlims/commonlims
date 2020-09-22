bbbf5d0efc3e918d39a176282eaad853243e2e49
Edvard Englund <edvard.englund@medsci.uu.se>
2020-09-22 13:24:45 +0200

    Workbatchfile available from workbatch by file handle
    Also, domain logic for workbatch is moved to service layer.

--- src/clims/services/workbatch.py
+++ src/clims/services/workbatch.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import
+from sentry.models.file import File
+from clims.services.file_handling.file_service import FILENAME_RE, FileNameValidationError
+from clims.models.workbatchfile import WorkBatchFile as WorkBatchFileModel
 
 
 class WorkbatchService:
@@ -12,3 +15,73 @@ class WorkbatchService:
     def get_step_template(self, name):
         from clims.utils import single
         return single([step for step in self.step_templates if step.name == name])
+
+
+class WorkBatch(object):
+    def __init__(self, work_batch_model):
+        self.archetype = work_batch_model
+
+    def add_file(self, file_stream, name, file_handle):
+        if FILENAME_RE.search(name):
+            raise FileNameValidationError('File name must not contain special whitespace characters')
+        file_model = File()
+        file_model.save()
+        file_stream.seek(0)
+        file_model.putfile(file_stream)
+        work_batch_file = WorkBatchFileModel(
+            organization=self.organization, work_batch=self.archetype,
+            name=name, file=file_model, file_handle=file_handle
+        )
+        work_batch_file.save()
+        self.archetype.files.add(work_batch_file)
+        self.save()
+
+    def get_single_file(self, file_handle=None):
+        from clims.utils import single_or_default, UnexpectedLengthError
+        try:
+            file_model = single_or_default(
+                [f for f in self.archetype.files.all() if f.file_handle == file_handle]
+            )
+        except UnexpectedLengthError:
+            raise ValueError(
+                "There were more than one file in this workbatch, "
+                "when it's expected to be only one! "
+                "file-handle : {}".format(file_handle))
+        if not file_model:
+            raise ValueError(
+                "There were no file in this workbatch, "
+                "file-handle: {}".format(file_handle))
+        return WorkBatchFile(file_model)
+
+    @classmethod
+    def create(cls, organization):
+        from clims.models.work_batch import WorkBatch as WorkBatchModel
+        model = WorkBatchModel(organization=organization)
+        model.save()
+        return WorkBatch(model)
+
+    @property
+    def organization(self):
+        return self.archetype.organization
+
+    def save(self):
+        self.archetype.save()
+
+
+class WorkBatchFile(object):
+    def __init__(self, work_batch_file_model):
+        self.archetype = work_batch_file_model
+
+    @property
+    def contents(self):
+        with self.archetype.file.getfile() as fp:
+            c = fp.read()
+        return c
+
+    @property
+    def file_stream(self):
+        return self.archetype.file.getfile()
+
+    @property
+    def name(self):
+        return self.archetype.name
---
+++ tests/clims/models/test_work_batch.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+import pytest
+from sentry.testutils import TestCase
+from clims.services.workbatch import WorkBatch
+from StringIO import StringIO
+
+
+class TestWorkBatch(TestCase):
+    def test__add_file_to_work_batch__file_is_fetched_from_workbatch_by_file_handle(self):
+        # Arrange
+        work_batch = WorkBatch.create(organization=self.organization)
+        file_stream = StringIO('contents')
+
+        # Act
+        work_batch.add_file(
+            file_stream=file_stream,
+            name='abc',
+            file_handle='file-handle'
+        )
+        fetched_file = work_batch.get_single_file(file_handle='file-handle')
+
+        # Assert
+        assert fetched_file.contents == 'contents'
+
+    def test_fetch_workbatchfile__when_there_is_none__excpetion(self):
+        # Arrange
+        work_batch = WorkBatch.create(organization=self.organization)
+
+        # Act
+        # Assert
+        with pytest.raises(ValueError):
+            work_batch.get_single_file(file_handle='file-handle')
+
+    @pytest.mark.dev_edvard
+    def test_fetch_single_workbatchfile__when_two_files_were_added__excpetion(self):
+        # Arrange
+        work_batch = WorkBatch.create(organization=self.organization)
+        file_stream = StringIO('contents')
+        work_batch.add_file(file_stream=file_stream, name='abc1', file_handle='file-handle')
+        work_batch.add_file(file_stream=file_stream, name='abc2', file_handle='file-handle')
+
+        # Act
+        # Assert
+        with pytest.raises(ValueError):
+            work_batch.get_single_file(file_handle='file-handle')
