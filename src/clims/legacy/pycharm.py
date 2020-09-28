7f2fe68e58e3cf3cb06bf36f8ad036cc8e34007f
Edvard Englund <edvard.englund@medsci.uu.se>
2020-09-28 12:05:37 +0200

    Remove clarity-ext legacy directory

--- src/clims/legacy/pycharm.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-"""Methods specific for generating pycharm run configurations"""
-from integration import ConfigFromConventionProvider
-
-
-def generate_pycharm_run_config(module):
-    # TODO: Implement
-    config_obj = ConfigFromConventionProvider.get_config_by_convention(module)
