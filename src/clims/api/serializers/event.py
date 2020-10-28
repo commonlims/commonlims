0428d7a5730c102b01bef6add980180bf92b3a0f
Edvard Englund <edvard.englund@medsci.uu.se>
2020-10-28 09:22:53 +0100

    Rename endpoint trigger-script to event
    Referenc a work definition by its full name (not db id) and retrieve it from the _implementation stack.

--- src/clims/api/serializers/event.py
+++ src/clims/api/serializers/event.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from rest_framework import serializers
 
 
-class ScriptTriggerSerializer(serializers.Serializer):
-    name = serializers.CharField()
+class EventSerializer(serializers.Serializer):
+    full_name = serializers.CharField()
     event_type = serializers.CharField()
     event_tag = serializers.CharField()
