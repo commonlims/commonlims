cfef1d74487549652bb03377a8bcc6465cef7b46
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2020-11-03 09:37:16 +0100

    CLIMS-421 - implement PR feedback and renumber migration

--- src/clims/models/transition.py
+++ src/clims/models/transition.py
@@ -15,23 +15,14 @@ class Transition(Model):
     """
     __core__ = True
 
-    BACKEND_CAMUNDA = 'camunda'
-
-    # Added because it seems to be used in all CLIMS models.
-    # TODO: check if it is actually needed?
-    organization = FlexibleForeignKey('sentry.Organization')
-
     # Added in case we want to be able to audit a user's history directly.
     user = FlexibleForeignKey('sentry.User', related_name='transitions')
 
     # The work batch in which this transition was created, if it was created in a work batch context.
-    # TODO: should we disallow nulls here? Are transitions ever created outside a work batch context?
     work_batch = FlexibleForeignKey('WorkBatch', null=True, related_name='transitions')
 
     source_location = FlexibleForeignKey('SubstanceLocation', related_name='source_transitions')
     target_location = FlexibleForeignKey('SubstanceLocation', related_name='target_transitions')
-    source_substance = FlexibleForeignKey('Substance', related_name='source_transitions')
-    target_substance = FlexibleForeignKey('Substance', related_name='target_transitions')
 
     transition_type = models.IntegerField(default=TransitionType.MOVE)
 
