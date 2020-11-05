0946b60b5f0dbff8a86302600484621311ee9a50
Steinar Sturlaugsson <costeinar@gmail.com>
2020-11-05 09:23:20 +0100

    rebo

--- src/clims/api/serializers/models/workunit.py
+++ src/clims/api/serializers/models/workunit.py
@@ -1,15 +1,17 @@
 from __future__ import absolute_import
 
 from rest_framework import serializers
+from clims.api.serializers.models.substance import SubstanceSerializer
 
 
 class WorkUnitSerializer(serializers.Serializer):
-    id = serializers.CharField(source="flexible_id")
-    tracked_object_global_id = serializers.SerializerMethodField(required=False)
+    id = serializers.IntegerField()
+    tracked_object = serializers.SerializerMethodField(required=False)
     external_work_unit_id = serializers.CharField(allow_null=True, required=False)
     external_workflow_instance_id = serializers.CharField(allow_null=True, required=False)
     workflow_provider = serializers.CharField()
     work_type = serializers.CharField()
 
-    def get_tracked_object_global_id(self, obj):
-        return obj.tracked_object_global_id
+    def get_tracked_object(self, obj):
+        print("ME HERE", obj.tracked_object, type(obj.tracked_object))
+        return SubstanceSerializer(obj.tracked_object).data
