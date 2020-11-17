48929c74f1b9d38958c326bad9376a7e8c42dd47
Edvard Englund <edvard.englund@medsci.uu.se>
2020-11-17 12:51:34 +0100

    Modify extensible property serializer

--- src/clims/api/serializers/models/extensible_property.py
+++ src/clims/api/serializers/models/extensible_property.py
@@ -5,11 +5,6 @@ from rest_framework import serializers
 
 
 class ExtensiblePropertySerializer(serializers.Serializer):
-    id = serializers.CharField(read_only=True)
-    version = serializers.IntegerField(read_only=True)
     name = serializers.CharField(read_only=True)
     display_name = serializers.CharField(read_only=True)
-    value = serializers.SerializerMethodField(method_name="get_value_with_correct_type")
-
-    def get_value_with_correct_type(self, obj):
-        return obj.value
+    value = serializers.CharField()
