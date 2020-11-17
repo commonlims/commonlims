9ba7823e2ed8c58df46262191775fa7040185249
Edvard Englund <edvard.englund@medsci.uu.se>
2020-11-17 15:09:05 +0100

    Workbatch serializer for updating

--- src/clims/api/serializers/models/work_batch_details.py
+++ src/clims/api/serializers/models/work_batch_details.py
@@ -7,4 +7,12 @@ from clims.api.serializers.models.extensible_property import ExtensiblePropertyS
 class WorkBatchDetailsSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(read_only=True)
-    properties = DictField(child=ExtensiblePropertySerializer(read_only=True))
+    properties = DictField(child=ExtensiblePropertySerializer(), allow_null=True)
+
+    def update(self, instance, validated_data):
+        if validated_data['properties'] is None:
+            return instance
+        for p in validated_data['properties']:
+            setattr(instance, p, validated_data['properties'][p]['value'])
+        instance.save()
+        return instance
